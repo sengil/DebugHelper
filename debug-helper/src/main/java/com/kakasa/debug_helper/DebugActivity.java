32657b3bb720b306e9ddcd77a1496a6f71d5c5a3
sengil <841250638@qq.com>
2018-04-28 18:29:47 +0800

    小bug

--- debug-helper/src/main/java/com/kakasa/debug_helper/DebugActivity.java
+++ debug-helper/src/main/java/com/kakasa/debug_helper/DebugActivity.java
@@ -1,7 +1,7 @@
 package com.kakasa.debug_helper;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Pair;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +16,7 @@ import java.util.List;
  * Created by Seng on 2018/4/26.
  */
 
-public class DebugActivity extends Activity implements View.OnClickListener {
+public class DebugActivity extends AppCompatActivity implements View.OnClickListener {
     TextView tv_current_base;
     TextView btn_switch_base;
     TextView tv_current_user;
@@ -25,7 +25,7 @@ public class DebugActivity extends Activity implements View.OnClickListener {
     private AccountAdapter mAccountAdapter;
 
     @Override
-    protected void onCreate( Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_debug);
         tv_current_base = findViewById(R.id.tv_current_base);
@@ -43,7 +43,9 @@ public class DebugActivity extends Activity implements View.OnClickListener {
             btn_switch_base.setText("切换到测试");
         }
 
-        tv_current_user.setText("当前用户："+DebugHelper.getInstance().getCurrentUser().first);
+        Pair<String, String> currentUser = DebugHelper.getInstance().getCurrentUser();
+        if (currentUser != null)
+            tv_current_user.setText("当前用户：" + currentUser.first);
         List<Pair<String, String>> userAccoutnts = DebugHelper.getInstance().getAccounts();
         if (userAccoutnts != null) {
             mAccountAdapter = new AccountAdapter(userAccoutnts);
@@ -62,7 +64,7 @@ public class DebugActivity extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
 
-        if(v.getId() == R.id.layout_switch_base){
+        if (v.getId() == R.id.layout_switch_base) {
             DebugHelper.getInstance().switchServer();
         }
 //        switch (v.getId()) {
@@ -98,7 +100,7 @@ public class DebugActivity extends Activity implements View.OnClickListener {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             TextView item = new TextView(DebugActivity.this);
-            item.setPadding(0,30,0,30);
+            item.setPadding(0, 30, 0, 30);
             Pair<String, String> pair = mData.get(position);
             item.setText("账号：" + pair.first + "\n密码：" + pair.second);
             return item;
